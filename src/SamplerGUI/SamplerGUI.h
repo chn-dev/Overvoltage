96271a1a07e6b45f7db4e67411a3e6b8cc5eaae8
Christian Nowak <chnowak@web.de>
2024-01-14 13:44:51 +0100

    restruct

---
+++ src/SamplerGUI/SamplerGUI.h
@@ -0,0 +1,13 @@
+#ifndef __SAMPLERGUI_H__
+#define __SAMPLERGUI_H__
+
+#include "AEGUISection.h"
+#include "EGUISection.h"
+#include "Keyboard.h"
+#include "NameRangesUISection.h"
+#include "OutputUISection.h"
+#include "SamplerKeyboard.h"
+#include "SampleUISection.h"
+#include "WaveView.h"
+
+#endif
